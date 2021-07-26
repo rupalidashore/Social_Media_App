263c15f65281cc2c02396cca9623be4a17107698
amankaur2004 <amankaur2004@gmail.com>
2021-07-26 15:09:15 -0700

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@
 module.exports = 
 
 {
-    mongoURI : 'mongodb+srv://ak:HotSauce@devconnector.amvtf.mongodb.net/devconnectordb?retryWrites=true&w=majority',
+    mongoURI : 'mongodb+srv://ak:@devconnector.amvtf.mongodb.net/devconnectordb?retryWrites=true&w=majority',
 secretOrKey : 'secret'
 
 }
